c39c117f3c20c2a51cd6668213972052e32be559
Chen Hao 陈浩 <chenhao@chexiang.com>
2016-04-19 18:17:50 +0800

    提交scrollView

--- SteadicamEVO/Class/Controllers/MainControllers/GuideVIewController/GuideScrollView/GifScrollView.m
+++ SteadicamEVO/Class/Controllers/MainControllers/GuideVIewController/GuideScrollView/GifScrollView.m
@@ -12,7 +12,7 @@
 
 @interface GifScrollView()
 
-@property (nonatomic, strong) NSMutableArray *subViews;
+@property (nonatomic, strong) NSMutableArray<GifViews *> *subViews;
 
 /**
  *  选择视图
@@ -52,22 +52,62 @@
 {
     _subViews = [[NSMutableArray alloc]init];
     
+    if (self.dataArray)
+    {
+        [self.dataArray enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            GifViews * gifView = [[GifViews alloc]init];
+            
+            gifView.moviePath = obj;
+            
+            [self addSubview:gifView];
+            
+            [self.subViews addObject:gifView];
+            
+        }];
+    }
+    
+    self.chooseView = [[PrepareChooseView alloc]initWithLeft:@"Later"
+                                                       right:@"Later"
+                                                       title:@"would you like to balance now?"];
+    [self addSubview:self.chooseView];
+    
     
 }
 
 -(void)autolayoutViews
 {
+    if (self.subViews)
+    {
+        
+    }
+    
     
 }
 
 -(void)bindData
 {
-    [RACObserve(self, dataArray) subscribeNext:^(id x) {
+    [RACObserve(self, dataArray) subscribeNext:^(NSArray *ary) {
         
+        NSInteger count = ary.count + [NSNumber numberWithBool:self.showChoose].integerValue;
         
+        self.contentSize                    = CGSizeMake(count* SCREEN_WIDTH, 0);
+        self.bounces                        = NO;
+        self.pagingEnabled                  = YES;
+        self.showsHorizontalScrollIndicator = NO;
+        
+        [self initViews];
+        
+        [self autolayoutViews];
     }];
     
-    [RACObserve(self, showChoose) subscribeNext:^(id x) {
+    [RACObserve(self, showChoose) subscribeNext:^(NSNumber *show) {
+        
+        NSInteger count = self.dataArray.count + [NSNumber numberWithBool:show].integerValue;
+        
+        self.contentSize = CGSizeMake(count* SCREEN_WIDTH, 0);
+        
+        self.chooseView.hidden = show;
         
     }];
 }
